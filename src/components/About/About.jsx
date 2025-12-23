64691844570491ee013ad70ffdab9c359941228c
PrathamAghara <pratham97274@gmail.com>
2025-12-23 12:26:49 +0530

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/work_logo/profile.png'
+import profileImage from '../../assets/work_logo/my image.webp';
 
 const About = () => {
   return (
